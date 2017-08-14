b03837e0bda2852a544e61fb4b6aee9698a000a7
chak808 <30875250+chak808@users.noreply.github.com>
2017-08-14 09:07:30 -1000

    Add files via upload

---
+++ js1.js
@@ -0,0 +1,8 @@
+function sayHello() {
+  var userFname = prompt("What is your first name?");
+  var userMname = prompt("What is your middle name?");
+  var userLname = prompt("What is your last name?")
+  //Ask for the user name//
+  window.alert("Hello "+" "+userFname+" "+userMname+" "+userLname);
+  //say hello to user//
+}
